b7bcdc10e4af67011294f4a4e773e44f04687dc4
kevinsus <99kevin.wong99@gmail.com>
2023-10-02 16:10:16 +1100

    added header and fix navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './navbar.css'
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
 const Navbar = () => {
   return (
     <div className='navbar-links'>
       <div className='navbar-links_logo'>
-        <h3>Kevin.dev</h3>
+        <h1><span>K</span> evin.S</h1>
       </div>
       <div className='navbar-links_containers'>
         <ul>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './header.css'
+import hero from '../../assets/logo.jpg'
 
 const Header = () => {
   return (
     <div className='header-container'>
       <div className='header-container_left'>
-        /* <p>Welcome To <br />My Personal Portfolio</p> */
+        <h1>Welcome To My Personal <span>Portfolio</span></h1>
+        <h3>Software Developer</h3>
+        <hr />
+        <p>Hi, I'm Kevin Susanto. A passionate student based in Sydney, Australia</p>
+        <p>I aim to join a dynamic team where I can apply my coding proficiency and collaborative.</p>
+        <div className='header-container_btn'>
+          <a href='#Me'>Resume</a>
+          <a href='#Me'>Hire Me</a>
+        </div>
       </div>
       <div className='header-container_right'>
+        <img src={hero} alt='logo'/>
       </div>
     </div>
   )
